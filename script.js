69ce42597a9c8eb40de117e56bf9712aac03300c
damary0517 <143422264+damary0517@users.noreply.github.com>
2023-09-21 15:03:42 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function rainbowfishAnswer(){
-  var password = document.getElementById("rainbowfishQuestion")
-  var passwordText = password.value;
-  if(passwordText=="Aquaarus Spectrum"){
-    return true;
-  }
-  alert("Incorrect Answer! Try Again")
-  return false; 
-}
